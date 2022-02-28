98d77932bf87948565b573987d5c5908edbb1d16
옥인준 <injunock@mailplug.co.kr>
2022-02-28 10:43:42 +0900

    first commit

---
+++ WatchaAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchaAssignment
+//
+//  Created by 옥인준 on 2022/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
